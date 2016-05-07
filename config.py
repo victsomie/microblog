6df8d0026d2e5575448aa8ca8e0a32388ac172b5
victsomie <victsomie@yahoo.com>
2016-05-07 12:28:02 +0300

    added commentting to config.py file

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+WTF_CSRF_ENABLED = True #activates the cross-site request forgery prevention
+SECRET_KEY = 'you-will-never-guess' #create a cryptographic token that is used to validate a form. NB: This is only needed when CSRF is enabled

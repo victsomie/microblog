6ef6684049c731291082aa92fc581817eab0a3eb
victsomie <victsomie@yahoo.com>
2016-05-09 00:34:19 +0300

    added comment on general functionality odf downgrade scripts

--- db_downgrade.py
+++ db_downgrade.py
@@ -1,4 +1,7 @@
 #!flask/bin/python
+
+# This script will downgrade the database one revision. You can run it multiple times to downgrade several revisions.
+
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
@@ -6,3 +9,5 @@ v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print('Current database version: ' + str(v))
+
+

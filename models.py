1a20f0adc6a7f41d8b22a8047bedfe8e7d95a659
victsomie <victsomie@yahoo.com>
2016-05-08 23:18:22 +0300

    added comment lines

--- models.py
+++ models.py
@@ -5,5 +5,10 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
 
+    #User class that we just created contains several fields, defined as class variables. Fields are created as instances of the db.Column class, which takes the field type as an argument, plus other optional arguments that allow us, for example, to indicate which fields are unique and indexed.
+
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+    #__repr__ method tells Python how to print objects of this class. We will use this for debugging

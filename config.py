96df7e8f8f951908545cd7bfa45501e9877fe9d1
victsomie <victsomie@yahoo.com>
2016-05-08 13:53:44 +0300

    added some opeID providers

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 WTF_CSRF_ENABLED = True #activates the cross-site request forgery prevention
 SECRET_KEY = 'you-will-never-guess' #create a cryptographic token that is used to validate a form. NB: This is only needed when CSRF is enabled
 
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]

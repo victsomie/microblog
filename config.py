9bff7ccbbb98ccea3e38ae893f402df58afe3053
victsomie <victsomie@yahoo.com>
2016-05-08 13:51:05 +0300

    edits

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 WTF_CSRF_ENABLED = True #activates the cross-site request forgery prevention
 SECRET_KEY = 'you-will-never-guess' #create a cryptographic token that is used to validate a form. NB: This is only needed when CSRF is enabled
+

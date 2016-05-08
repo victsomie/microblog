7cfbcabada401aa8cca9140beed94eaa54f648ff
victsomie <victsomie@yahoo.com>
2016-05-08 22:48:01 +0300

    added comments to code

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ fform flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config') #tell Flask to read it and use it
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) #db is our databse
 
-from app import views, models
+from app import views, models #adds a new import of a module called models

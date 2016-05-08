6869a8213d7c67fe81345d69607d149c5e03f069
victsomie <victsomie@yahoo.com>
2016-05-08 22:40:54 +0300

    added comments to code

--- config.py
+++ config.py
@@ -12,5 +12,6 @@ OPENID_PROVIDERS = [
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')#required by the Flask-SQLAlchemy extension. This is the path of our database file.
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLALCHEMY_MIGRATE_REPO is the folder where we will store the SQLAlchemy-migrate data files

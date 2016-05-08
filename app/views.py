d985c0e3f926075c892680d34566af060c8d5595
victsomie <victsomie@yahoo.com>
2016-05-08 14:36:53 +0300

    added content

--- app/views.py
+++ app/views.py
@@ -58,6 +58,11 @@ def jlogin():
 #methods argument in the route decorator. This tells Flask that this view function accepts GET and POST requests
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])

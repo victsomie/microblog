87232beb50a5f25cc43ee2551024d0a13393ffa9
victsomie <victsomie@yahoo.com>
2016-05-08 22:45:51 +0300

    added code to initialize the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+fform flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config') #tell Flask to read it and use it
+db = SQLAlchemy(app)
 
-
-from app import views
+from app import views, models

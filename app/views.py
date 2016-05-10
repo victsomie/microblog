dca875873a10bc2c13f2347e6da732322a65fab1
victsomie <victsomie@yahoo.com>
2016-05-10 22:10:29 +0300

    added login view function

--- app/views.py
+++ app/views.py
@@ -61,12 +61,14 @@ def jlogin():
 """
 @app.route('/login', methods=['GET', 'POST'])
 #methods argument in the route decorator. This tells Flask that this view function accepts GET and POST requests
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,

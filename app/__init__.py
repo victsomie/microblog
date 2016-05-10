ab112287e19f7e6627a7711a922eae4f76f0c04c
victsomie <victsomie@yahoo.com>
2016-05-10 19:26:59 +0300

    added configuration for Flask-Login & Flask-OpenID

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+#login configuration here
+#---------
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+#--------------
+
+
+
 app = Flask(__name__)
 app.config.from_object('config') #tell Flask to read it and use it
 db = SQLAlchemy(app) #db is our databse
 
 from app import views, models #adds a new import of a module called models
+
+
